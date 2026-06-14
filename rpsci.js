0c54ab310d20b6d98a6ce7a4eec9a47513949813
YKs22k <Dasthebubbleboy@gmail.com>
2026-06-14 15:05:28 +0800

    Finish the project with extra input limit. .Add console for each roung recording and a final score remind

--- rpsci.js
+++ rpsci.js
@@ -1,43 +1,70 @@
 
 
 
-Print "Hello World" to confirm JavaScript is connected
+console.log("Hello World")
 
-#Create function getComputerChoice
-    Generate a random number from 0.1.2
-    0.1.2 corresponding to Rock Paper Scissors
-#Create function getHumanChoice(prompt(Please input))
-#Convert all input to first letter uppercase and rest lowercase.
+function getComputerChoice() {
 
-#If getHumanChoice > 0 
-            if getHumanchoice > getComputerChoice
-            return humanChoice += 1
-# Else if getHumanChoice = getComputerChoice
-            return Tie
-# Else if getHumanChoice = 0
-            if getcomputerChoice = 1
-            return computerChoice += 1
-            Else if getcomputerChoice = 2
-            return computerChoice += 1
-Create function 
-# Set computerScore and humanScore = 0
-    Create function playRound with humanChoice and computerChoice
-        If humanChoice is the same as computerChoice
-            Print "It's a tie"
-        Else if humanChoice beats computerChoice
-            Add 1 to humanScore
-            Print "You win"
-        Else
-            Add 1 to computerScore
-            Print "You lose"
-    Repeat 5 times
-        Get human choice
-        Get computer choice
-        Play one round
-    If humanScore is greater than computerScore
-        Print "You won the game"
-    Else if computerScore is greater than humanScore
-        Print "You lost the game"
-    Else
-        Print "The game is a tie"
-Call playGame
+    const computerChoice = Math.floor(Math.random() * 3);
+    return computerChoice;
+}
+
+function choiceName(choice) {
+  if (choice === 0) return "Rock";
+  if (choice === 1) return "Paper";
+  if (choice === 2) return "Scissors";
+}
+function getHumanChoice() {
+    const inputChoice = prompt("Choose Rock, Paper, or Scissors:");
+  const humanChoice =
+    inputChoice.charAt(0).toUpperCase() + inputChoice.slice(1).toLowerCase();
+    if (humanChoice === "Rock") {
+    return 0;
+  } else if (humanChoice === "Paper") {
+    return 1;
+  } else if (humanChoice === "Scissors") {
+    return 2;
+  } else {
+    alert('Invalid Choice! Please only enter from "Rock, Paper, or Scissors"');
+    return humanChoice;
+
+}
+}
+
+function play() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+function playRound(humanSelection, computerSelection) {
+
+    if(humanSelection === computerSelection) {
+        return console.log(`It's a tie! You both chose ${choiceName(computerSelection)}.`);
+    } else if( 
+      (humanSelection === 0 && computerSelection === 2) ||
+      (humanSelection === 1 && computerSelection === 0) ||
+      (humanSelection === 2 && computerSelection === 1)) {
+        humanScore++;
+        console.log(`You win! ${choiceName(humanSelection)} beats ${choiceName(computerSelection)}`);
+    }  else {
+        computerScore++;
+        console.log(`You lose! ${choiceName(computerSelection)} beats ${choiceName(humanSelection)}`);
+    }
+}
+
+  for (let round = 1; round <= 5; round++) {
+    console.log(`Round ${round}`);
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+  console.log(`Final score: Human ${humanScore} - Computer ${computerScore}`);
+
+  if (humanScore > computerScore) {
+    console.log("You won the game!");
+  } else if (computerScore > humanScore) {
+    console.log("You lost the game!");
+  } else {
+    console.log("The game is a tie!");
+  }
+}
+
+
+play();
