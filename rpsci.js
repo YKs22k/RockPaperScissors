e3eb212330c45aa1deee7a299ad13856eb8a1ab2
YKs22k <Dasthebubbleboy@gmail.com>
2026-06-14 12:33:31 +0800

    Write a pseudocode for the game.

---
+++ rpsci.js
@@ -0,0 +1,43 @@
+
+
+
+Print "Hello World" to confirm JavaScript is connected
+
+#Create function getComputerChoice
+    Generate a random number from 0.1.2
+    0.1.2 corresponding to Rock Paper Scissors
+#Create function getHumanChoice(prompt(Please input))
+#Convert all input to first letter uppercase and rest lowercase.
+
+#If getHumanChoice > 0 
+            if getHumanchoice > getComputerChoice
+            return humanChoice += 1
+# Else if getHumanChoice = getComputerChoice
+            return Tie
+# Else if getHumanChoice = 0
+            if getcomputerChoice = 1
+            return computerChoice += 1
+            Else if getcomputerChoice = 2
+            return computerChoice += 1
+Create function 
+# Set computerScore and humanScore = 0
+    Create function playRound with humanChoice and computerChoice
+        If humanChoice is the same as computerChoice
+            Print "It's a tie"
+        Else if humanChoice beats computerChoice
+            Add 1 to humanScore
+            Print "You win"
+        Else
+            Add 1 to computerScore
+            Print "You lose"
+    Repeat 5 times
+        Get human choice
+        Get computer choice
+        Play one round
+    If humanScore is greater than computerScore
+        Print "You won the game"
+    Else if computerScore is greater than humanScore
+        Print "You lost the game"
+    Else
+        Print "The game is a tie"
+Call playGame
